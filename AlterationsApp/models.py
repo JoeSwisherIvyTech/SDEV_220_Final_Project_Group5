bebbe596357506f5e61d007b3dbe1cd081632cd8
Joe Swisher <jswisher6@ivytech.edu>
2026-05-07 15:59:35 -0400

    order_manage, accept orders, back button

--- AlterationsApp/models.py
+++ AlterationsApp/models.py
@@ -52,6 +52,7 @@ class Order(models.Model):
     ]
 
     customer_name = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    assigned_staff = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL, related_name='assigned_staff')
     item = models.CharField(max_length=100, choices=ITEM_CHOICES)
     alteration_type = models.CharField(max_length=100, choices=ALTERATION_CHOICES)
     material = models.CharField(max_length=100, choices=MATERIAL_CHOICES)
@@ -64,6 +65,10 @@ class Order(models.Model):
     waist = models.FloatField(null=True, blank=True)
     hips = models.FloatField(null=True, blank=True)
     inseam = models.FloatField(null=True, blank=True)
+
+    def assign_staff(self, staff_name):
+        self.assigned_staff = staff_name
+        self.save()
 
     def __str__(self):
         return f"{self.id} - {self.customer_name} - {self.item}"
